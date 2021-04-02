0a60bdddab257b81f9e7abb45bdf30da2008a7da
Ben Friedman <55304085+benfriedman97@users.noreply.github.com>
2021-04-02 11:52:30 -0400

    Update about_us.php

--- pages/about_us.php
+++ pages/about_us.php
@@ -28,6 +28,34 @@
 
 <head>
     <?php include "../res/modules/header.php"; ?>
+
+    <style>
+        h1 {
+            text-align:center;
+        }
+        .about {
+            padding-top: 50px;
+        }
+        
+        .about a:link {
+            color: blue;
+            background-color: transparent;
+            text-decoration: underline;
+        }
+        
+       .about a:visited {
+            color: blue;
+            background-color: transparent;
+            text-decoration: underline;
+       }
+        
+        .about a:hover {
+            color: red;
+            background-color: transparent;
+            text-decoration: underline;
+        }
+        
+    </style>
 </head>
 
 <body>
@@ -45,9 +73,46 @@
 
             <!-- main content -->
             <div class="col-xs-12 col-sm-12 col-md-8">
-                <p>
+
+            <!-- Photo, Email, GitHub -->
+                <p style="text-align:center">
                     ABOUT US
                 </p>
+
+                <h1>This page needs more work</h1>
+                 <!-- Joe -->
+                 <div class="about">
+                    <h1>Joseph Calles - Full Stack Developer</h1>
+                    <p>Joseph Calles is an undergraduate computer science student at
+                        UMass Lowell graduating in the Spring of 2021. He will work as
+                        a software engineer with HCL Technologies after graduation.
+                    </p>
+                    <p>GitHub: <a href="https://github.com/JStar-C">JStar-C</a></p>
+                    <p>Email: <a href="mailto: joseph_calles@student.uml.edu">joseph_calles@student.uml.edu</a></p>
+                </div>
+
+                <!-- Tammy -->
+                <div class="about"> 
+                    <h1>Tammy Liu - Full Stack Developer</h1>
+                    <p>Tammy Liu is an undergraduate computer science and mathematics student at
+                        UMass Lowell graduating in the Spring of 2021. She will continue her
+                        studies at UMass Lowell after graduation by pursuing a masters degree
+                        in mathematics.
+                    </p>
+                    <p>GitHub: <a href="https://github.com/TammyLiuXD">TammyLiuXD</a></p>
+                    <p>Email: <a href="mailto: tammy_liu@student.uml.edu">tammy_liu@student.uml.edu</a></p>
+                </div>
+               
+                <!-- Ben -->
+                <div class="about">
+                    <h1>Benjamin Friedman - Full Stack Developer</h1>
+                    <p>Benjamin Friedman is an undergraduate computer science and mathematics student at
+                        UMass Lowell graduating in the Fall of 2022.
+                    </p>
+                    <p>GitHub: <a href="https://github.com/benfriedman97">benfriedman97</a></p>
+                    <p>Email: <a href="mailto: benjamin_friedman@student.uml.edu">benjamin_friedman@student.uml.edu</a></p>
+                </div>
+                
             </div>
 
             <!-- right sidebar -->
@@ -55,6 +120,10 @@
                 Right Sidebar
             </div>
             
+            <!-- About Us Descriptions -->
+            <div class="container-fluid">
+
+            </div>
         </div>
     </div>
 
