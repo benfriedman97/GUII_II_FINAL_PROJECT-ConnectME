6299fefec6f6344282904ddf171d7a33f796a847
Ben Friedman <55304085+benfriedman97@users.noreply.github.com>
2021-04-15 16:20:54 -0400

    Create browse_resources.php

---
+++ pages/browse_resources.php
@@ -0,0 +1,30 @@
+<?php include '../res/modules/prescript.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <?php include "../res/modules/header.php"; ?>
+
+    <!-- page-specific style -->
+    <style>
+
+    
+    </style>
+</head>
+<body>
+    
+    <?php include "../res/modules/navbar.php"; ?>
+
+    
+    
+    
+
+    <?php include "../res/modules/footer.php"; ?>
+    <?php include '../res/modules/postscript.php'; ?>
+</body>
+</html>
