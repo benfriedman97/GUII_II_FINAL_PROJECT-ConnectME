886986e20cf12ef0e9ef84f1bec4133621837113
Ben Friedman <55304085+benfriedman97@users.noreply.github.com>
2021-04-15 16:22:47 -0400

    Create explore.php

---
+++ pages/explore.php
@@ -0,0 +1,47 @@
+<!-- ---------------------------------------------------------------------- --
+  - Joseph Calles, Benjamin Friedman, & Tammy Liu
+  - University of Massachusetts Lowell
+  - COMP.4620-201 SP2021 Term Project
+  - Filename: index.html
+  - Link: <INSERT HERE>
+  - Email:  joseph_calles@student.uml.edu
+            benjamin_friedman@student.uml.edu
+            tammy_liu@student.uml.edu
+  - Copyright (c) 2021 by Josph Calles, Benjamin Friedman, & Tammy Liu. 
+        All rights reserved. May be freely copied or excerpted for educational 
+        purposes with credit to the author.
+  - File created on 03/19/2021.
+  - HTML file for the landing page of our project
+  - TO DO:
+        x add main body content
+        x make working sign in buttons
+        x redesign color scheme (https://bootswatch.com/cerulean/)
+        x integrate backend and php server
+  -- ---------------------------------------------------------------------- -->
+
+  <?php include '../res/modules/prescript.php'; ?>
+
+<!DOCTYPE html>
+
+<html lang="en-us">
+
+<head>
+    <?php include "../res/modules/header.php"; ?>
+</head>
+
+<body>
+
+    <?php include "../res/modules/navbar.php"; ?>
+
+    <!-- main body -->
+    <div class="container-fluid">
+        
+    </div>
+
+    <?php include "../res/modules/footer.php"; ?>
+
+    <?php include '../res/modules/postscript.php'; ?>
+
+</body>
+
+</html>
