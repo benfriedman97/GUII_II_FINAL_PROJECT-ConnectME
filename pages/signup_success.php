4b30486d80d7e732cd634dbfdcf3010f05794735
Ben Friedman <55304085+benfriedman97@users.noreply.github.com>
2021-05-02 17:16:06 -0400

    Create signup_success.php

---
+++ pages/signup_success.php
@@ -0,0 +1,33 @@
+<?php include '../res/modules/prescript.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <?php include "../res/modules/header.php"; ?>
+
+    <!-- page-specific style -->
+    <style>
+
+    
+    </style>
+</head>
+<body>
+    
+    <?php include "../res/modules/navbar.php"; ?>
+
+    <div class="container" style="text-align:center">
+    <h5>Your account has been successfully created! You can now login and
+        will receive email verification.</h5>
+    </div>
+    
+    
+
+    <?php include "../res/modules/footer.php"; ?>
+    <?php include '../res/modules/postscript.php'; ?>
+</body>
+</html>
