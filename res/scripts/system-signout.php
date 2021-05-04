5d49f6f9aefe6dd37994f928e4254a588795d05b
Joseph Calles <62597878+JStar-C@users.noreply.github.com>
2021-05-03 20:12:18 -0400

    Add files via upload

--- res/scripts/system-signout.php
+++ res/scripts/system-signout.php
@@ -15,9 +15,20 @@ function systemLog($msg) {
 systemLog($_SESSION["current-user-email"] . " signed out");
 
 // unset session variables
-unset($_SESSION["current-user-email"]);
-unset($_SESSION["current-user-password"]);
-unset($_SESSION["current-user-first-name"]);
+unset($_SESSION["current-user-user-id"])       ;
+unset($_SESSION["current-user-first-name"])    ;
+unset($_SESSION["current-user-last-name"])     ;
+unset($_SESSION["current-user-date-of-birth"]) ;
+unset($_SESSION["current-user-address"])       ;
+unset($_SESSION["current-user-city"])          ;
+unset($_SESSION["current-user-state"])         ;
+unset($_SESSION["current-user-account-type"])  ;
+unset($_SESSION["current-user-univ-or-comp"])  ;
+unset($_SESSION["current-user-email"])         ;
+unset($_SESSION["current-user-password"])      ;
+unset($_SESSION["current-user-is-verified"])   ;
+unset($_SESSION["current-user-interests"])     ;
+unset($_SESSION["current-user-validation-key"]);
 $_SESSION["signed-in"] = false;
 
 $link = 'http://' . $_SERVER["HTTP_HOST"] . 
